caac2ed4cb46059f8c82f3983126d32bc66a5bb3
FerraBraiZ <ferrabraiz@gmail.com>
2019-11-18 00:43:41 -0300

    wip

--- app/controllers/PostFileSignerController.php
+++ app/controllers/PostFileSignerController.php
@@ -1,152 +1,132 @@
 <?php
 
+require_once('../../vendor/autoload.php');
+ini_set('ignore_repeated_errors', 'On');
+ini_set('html_errors', 'On');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+date_default_timezone_set('America/Sao_Paulo');
+
+use Aws\S3\S3Client;
+use Aws\S3\PostObjectV4;
+use Aws\Exception\AwsException;
+
 class PostFileSignerController extends ControllerBase {
-    
-    protected $number_of_uploaded_images;
-    protected $uploaded;
-    protected $json_data;
-    protected $error;
-    protected $upload_error_msg;
-    protected $upload_folder = 'uploads/';
-    protected $allowed_types = 'jpeg|jpg|png';
-    
-//------------------------------------------------------------------------------
-
-    public function __construct() {
+ 
+    public function index() {
         
-        parent::__construct();
+       try {
 
-        $this->load->model('Foto_model','foto_model');        
-        $this->load->library('upload');        
-        $this->load->library('image_lib');
+    if( isset( $_SERVER['HTTP_ORIGIN'] ) && $_SERVER['HTTP_ORIGIN'] != '' )
+    {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 0');    // cache for 0 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if( $_SERVER['REQUEST_METHOD'] == 'OPTIONS')
+    {
+        if( isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
+        if( isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        exit;
+    }
+
+    if ('POST' !== $_SERVER['REQUEST_METHOD'] ) {
+
+        http_response_code(405);
+        exit( "Method not allowed!");
+
+    }
+
+    $stdin = (object)\GuzzleHttp\json_decode( file_get_contents("php://input"), true );
+
+    if( !isset( $stdin ) ) {
+
+        http_response_code(422);
+        exit( "no content was received on this endpoint, looks like you forgot something!" );
+
+    }
+
+    [ 'fileName' => $fileName, 'folder' => $folder, 'uuid' => $uuid  ] = get_object_vars($stdin);
+
+    $filename = "$folder/$uuid/$fileName";
+
+
+
+
+    $client = new S3Client([
+        'version' => 'latest',
+        'region' => 'sa-east-1',
+//        'profile' => 'default', it would read credentials from /home/${USER}/.aws/credentials
+        'credentials' => [
+            'key'    => AWS_KEY,
+            'secret' => AWS_SECRET,
+        ],
+    ]);
+
+    // Set some defaults for form input fields
+    $formInputs = ['acl' => 'private'];
+
+    // Construct an array of conditions for policy
+    $options = [
+        ["acl" => "private"],
+        ["bucket" => AWS_BUCKET],
+        ["content-type"=>""],
+        ["key"=>"${filename}"],
+        ["success_action_status"=>"201"]
+    ];
+
+    // Optional: configure expiration time string
+    $expires = '+1 hours';
+
+    $postObject = new PostObjectV4(
+        $client,
+        AWS_BUCKET,
+        $formInputs,
+        $options,
+        $expires
+    );
+
+    // Get attributes to set on an HTML form, e.g., action, method, enctype
+    $formAttributes = $postObject->getFormAttributes();
+
+    // Get form input fields. This will include anything set as a form input in
+    // the constructor, the provided JSON policy, your AWS access key ID, and an
+    // auth signature.
+    $formInputs = $postObject->getFormInputs();
+
+    header('Content-Type:application/json');
+    http_response_code(200);
+    die(json_encode([
+        "contentType" => ["content-type"=>""],
+        "acl" => [ "acl"=>"private" ],
+        "successActionStatus" => ["success_action_status"=>"201"],
+        "policy" => ["policy"=>$formInputs["Policy"]],
+        "credential" => ["X-amz-credential"=>$formInputs["X-Amz-Credential"]],
+        "algorithm" => ["x-amz-algorithm"=>$formInputs["X-Amz-Algorithm"]],
+        "date" => ["x-amz-date"=>$formInputs["X-Amz-Date"]],
+        "signature" => ["x-amz-signature"=>$formInputs["X-Amz-Signature"]],
+        "key" => ["key"=>"${filename}"],
+        "postEndpoint" => ["postEndpoint"=>"https://ferrabraiz-test-bucket.s3.amazonaws.com/"]
+    ]));
+
+} catch (AwsException $e) {
+
+    header('Content-Type:application/json');
+    http_response_code(200);
+    die(json_encode(["AwsException" => $e->getMessage()]));
+
+} catch (\Throwable $e) {
+
+    header('Content-Type:application/json');
+    http_response_code(200);
+    die(json_encode(["error" => $e->getMessage()]));
+}
         
     }
 
-//------------------------------------------------------------------------------
-    
-    public function upload($allowed_number_of_files_per_upload) {
-
-        if ( empty( $_FILES ) ) {
-            $this->output->set_output(json_encode(array('result' => 0, '' => 'Erro! Selecione ao menos uma foto para enviar')));
-            return false;
-        }
-
-        $this->number_of_uploaded_images = count($_FILES['uploadedimages']['tmp_name']);
-
-        if ($this->number_of_uploaded_images > $allowed_number_of_files_per_upload) {
-            
-            $this->upload_error_msg = $allowed_number_of_files_per_upload === 1 ? 
-                    "Erro! &#201; permitido apenas 1 foto por envio." : 
-                    "Erro! &#201; permitido apenas 10 fotos por envio.";           
-            
-            $this->output->set_output(json_encode(array('result' => 0, '' => $this->upload_error_msg)));
-            
-            return false;
-        }
-
-        $temp = $_FILES['uploadedimages'];
-
-        for ($i = 0; $i < $this->number_of_uploaded_images; $i++) {
-            
-            if ($_FILES['uploadedimages']['error'][$i] != 0) {
-                
-                $this->error[$i] = $_FILES['uploadedimages']['error'][$i];
-                
-            }
-            
-        }
-        
-        if( !empty( $this->error ) ){
-            
-            $this->output->set_output( json_encode( array( 'result' => 0, '' => show( $this->error ) ) ) );
-
-            return false;
-            
-        }
-        
-        $config['file_name'] = \uniqid() . "_" . \Date('d-m-Y_H-i-s');
-        $config['upload_path'] = \FCPATH . $this->upload_folder;
-        $config['allowed_types'] = $this->allowed_types;
-        $created_images = array();
-
-        for ($i = 0; $i < $this->number_of_uploaded_images; $i++) {
-            
-            $_FILES['uploadedimage']['name'] = $temp['name'][$i];
-            $_FILES['uploadedimage']['type'] = $temp['type'][$i];
-            $_FILES['uploadedimage']['tmp_name'] = $temp['tmp_name'][$i];
-            $_FILES['uploadedimage']['error'] = $temp['error'][$i];
-            $_FILES['uploadedimage']['size'] = $temp['size'][$i];
-
-            $this->upload->initialize($config);
-            if ($this->upload->do_upload('uploadedimage')) {
-                
-                $created_images[$i] = $this->uploaded[$i] = $this->upload->data();
-                
-            } else {
-                
-                $this->error[$i] = $this->upload->display_errors('', '');
-                
-                $this->output->set_output(json_encode(array('result' => 0, '' => $this->error[$i])));
-                
-                unset($this->error[$i]);
-                
-                return false;
-                
-            }
-            
-        }
-
-        $configs = array();
-
-        foreach ($created_images as $key => $source_image) {
-
-            $configs[$key] = array('source_image' => $source_image['file_name'], 
-                                   'new_image' => 'thumbs-' . $source_image['file_name'], 
-                                   'width' => 218, 'height' => 257, 'maintain_ratio' => FALSE);
-            
-        }
-
-        foreach ($configs as $key => $config) {
-            //$this->image_lib->_enlarge(true);
-            $this->image_lib->thumb($config, FCPATH . $this->upload_folder);
-            $this->json_data[$key] = ([ 'result' => 1, 'file_name' => $config['new_image']]);
-        }
-
-        echo json_encode($this->json_data, JSON_UNESCAPED_SLASHES);
-
-        unset($_FILES);unset($temp);unset($this->uploaded);
-        
-        return false;
-        
-    }/* upload()*/
-    
-//------------------------------------------------------------------------------
-
-    public function deleteUploadedFile($foto) {
-        //SoftDelete
-        if ( !is_int( $this->foto_model->update( array( 'status' => '0' ), array( 'foto_nome' => $foto ) ) ) ) {
-
-            log_message( 'error','softDelete falhou! '.$this->stacktrace,true );
-
-            return false;    
-
-        }
-     
-//        if ( $this->foto_model->deleteFotoFromDiskAndDB($foto) === false ) {
-//
-//            $error_msg = 'Erro! O arquivo que voc&#234; est&#225; tentado remover n&#227;o existe ou j&#225; fora removido!';
-//
-//            $this->output->set_output(json_encode(array('result' => 0, 'erro' => $error_msg)));
-//
-//            return false;
-//        }
-
-        $this->output->set_output(json_encode(array('result' => 1, 'deleted_file_name' => $foto)));
-
-        return false;
-        
-    }    
-   
-//------------------------------------------------------------------------------
-    
+ 
 }
